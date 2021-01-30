1396341a40e441df2cb2728b69c22ad7ae9e6d7b
WarakornToey <warakorn_tei@hotmail.com>
2021-01-30 20:43:06 +0700

    Lecture_50

---
+++ Lecture_50_Warakorn_L.py
@@ -0,0 +1,15 @@
+def positiveNumber(x,y):
+    print(x+y)
+positiveNumber(100,25)
+
+def deleteNumber(x,y):
+    print(x-y)
+deleteNumber(100,25)
+
+def multiplyeNumber(x,y):
+    print(x*y)
+multiplyeNumber(100,25)
+
+def divideNumber(x,y):
+    print(x/y)
+divideNumber(100,25)
